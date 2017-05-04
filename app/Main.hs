e61fec135cc964fa07a049fa6449c2975fd64dd0
andy <andy@ashall.co>
2017-05-04 20:29:18 +0100

    testing json files

--- app/Main.hs
+++ app/Main.hs
@@ -1,7 +1,38 @@
-module Main where
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DeriveAnyClass #-}
 
-import Lib
+import GHC.Generics
+import Data.Aeson
+import Data.Text (Text)
+import qualified Data.ByteString.Lazy as B
+
+data Date = Date { date :: Text } deriving (Show, Generic)
+
+data Post = Post { _id :: Text
+                 , title :: Text
+                 , url :: Text
+                 , created :: Date
+                 , subject :: Text
+                 , summary :: Text
+                 , content :: Text
+                 , likes :: Int } deriving (Show, Generic)
+
+instance FromJSON Post
+instance ToJSON Post
+
+instance FromJSON Date
+instance ToJSON Date
+
+jsonFile :: FilePath
+jsonFile = "projects.json"
+
+getJSON :: IO (B.ByteString)
+getJSON = B.readFile jsonFile
 
 main :: IO ()
 main = do
-	someFunc
+	d <- (eitherDecode <$> getJSON) :: IO (Either String [Post])
+        case d of
+            Left err -> putStrLn err
+            Right ps -> print ps
